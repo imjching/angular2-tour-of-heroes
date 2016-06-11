4b9f1247e4ed04c13f35b125b2a384721ed1a4c4
Jay <jay@imjching.com>
2016-06-11 15:02:01 +0800

    http

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -12,13 +12,17 @@ import { HeroService } from './hero.service';
 
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
+
   constructor(
     private router: Router,
     private heroService: HeroService) {
   }
+
   ngOnInit() {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1,5));
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1,5));
   }
+
   gotoDetail(hero: Hero) {
     let link = ['HeroDetail', { id: hero.id }];
     this.router.navigate(link);
